27f2884a2978b4d90d9124a8fe271a1fec81f0d8
Joshua Baker <joshuab2@discovery.co.za>
2021-05-11 16:33:28 +0200

    Add calculator screen

--- tax-calculator-frontend/src/app/app-routing.module.ts
+++ tax-calculator-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TaxCalculatorComponent} from "./tax-calculator/tax-calculator.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/calculator', pathMatch: 'full' },
+    {
+        path: 'calculator',
+        component: TaxCalculatorComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
